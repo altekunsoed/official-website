3979303241524d8d1e50679782e0dd0e6cd55727
Shafnaa <saujanashafi2003@gmail.com>
2024-03-31 19:53:32 +0700

    feat: adding assets

---
+++ assets/divisions/index.ts
@@ -0,0 +1,7 @@
+import humkominfo from "./humkominfo.png";
+import ttl from "./ttl.png";
+import ttk from "./ttk.png";
+import tki from "./tki.png";
+import tik from "./tik.png";
+
+export { humkominfo, ttl, ttk, tki, tik };
--- assets/index.ts
+++ assets/index.ts
@@ -1,5 +1,6 @@
 import OfficialLogo from "./OfficialLogo";
 import TextualLogo from "./TextualLogo";
 import unsoedVector from "./unsoedVector.png";
+import cable from "./cable.png";
 
-export { OfficialLogo, TextualLogo, unsoedVector };
+export { OfficialLogo, TextualLogo, unsoedVector, cable };
