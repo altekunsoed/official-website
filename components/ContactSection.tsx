eb0ce457a8c659984b91b825f07136025342ac78
Shafnaa <saujanashafi2003@gmail.com>
2024-03-31 19:57:55 +0700

    feat: adding faq & contact section

---
+++ components/ContactSection.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { cable } from "@/assets";
+
+const ContactSection = () => {
+  return (
+    <section className="w-full px-1 md:px-12">
+      <div className="relative w-full h-96 flex flex-col bg-black rounded-3xl justify-center items-center gap-6 overflow-hidden">
+        <Image
+          src={cable}
+          alt=""
+          className="absolute w-full h-full object-cover object-center"
+        />
+        <div className="absolute w-full h-full bg-black/20"></div>
+        <h2 className="relative text-6xl font-bold">Lets talk.</h2>
+        <Link href={"mailto:soedirmanrobotic@gmail.com"}>
+          <Button
+            variant={"default"}
+            size={"lg"}
+            className="relative text-base px-12"
+          >
+            Get In Touch
+          </Button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default ContactSection;
---
+++ components/FaqSection.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import SectionTitle from "@/components/SectionTitle";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { frequentQuestion } from "@/lib/question";
+
+const FaqSection = () => {
+  return (
+    <section className="flex w-full px-4 md:px-10 py-20 flex-col justify-center items-start gap-12">
+      <SectionTitle
+        title="Frequently Asked Questions"
+        desc="Still have question about us? You might be able to find your answer here."
+      />
+      <Accordion type="multiple" className="w-full md:px-12">
+        {frequentQuestion.map((question, index) => (
+          <AccordionItem value={`item-${index}`} key={index}>
+            <AccordionTrigger>{question.question}</AccordionTrigger>
+            <AccordionContent>{question.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </section>
+  );
+};
+
+export default FaqSection;
